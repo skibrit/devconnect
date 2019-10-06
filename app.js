ddc5c1117172970e4ebc583087a3aca5bafdbf65
Saidul Amin <Full Stack Web Developer>
2019-10-06 22:00:56 +0600

    prepare for deploy

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const app = express();
 const Routes = require("./routeList");
+const path = require("path");
 const DbConnected = require("./config/dbConnecter");
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
 //connect to mongo database
 DbConnected();
 
 //add middleware parse
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extended: false }));
 
 //connect routes from routeList
-app.use("/api/registration",Routes.registration);
-app.use("/api/auth",Routes.auth);
-app.use("/api/profile",Routes.profile);
-app.use("/api/post",Routes.post);
+app.use("/api/registration", Routes.registration);
+app.use("/api/auth", Routes.auth);
+app.use("/api/profile", Routes.profile);
+app.use("/api/post", Routes.post);
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening on ${PORT}`);
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
 });
