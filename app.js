416fb3630cbe4b027ef60889d21621073ac786ec
Saidul Amin <Full Stack Web Developer>
2019-10-06 22:20:27 +0600

    updated

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use("/api/post", Routes.post);
 if (process.env.NODE_ENV == "production") {
 }
 
-app.use("/", express.static("client/build"));
-/*
+app.use(express.static("client/build"));
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});*/
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
